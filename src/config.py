bbb03b8f0ad5028fccf2b6f22813438e67c50422
Swarna-N-T <146638999+Swarna-N-T@users.noreply.github.com>
2025-09-20 08:19:10 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,9 @@
+# Configuration variables
+
+IMAGE_SIZE = 64
+
+# Paths
+ZIP_PATH = '/content/drive/Mydrive/ICT/dataset/PlantVillage.zip'
+EXTRACT_PATH = '/content/drive/Mydrive/ICT/dataset/'
+DATASET_PATH = "/content/drive/MyDrive/Colab Notebooks/internship project/PlantVillage"
+MODEL_PATH = "/content/drive/MyDrive/ICT/cnn_model.h5"
